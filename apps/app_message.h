24c9e1ae7b514791d90f87137c66b92b69d0b3a4
JackZhao2017 <kebe2014zyp@126.com>
2017-06-26 12:02:04 +0800

    create example

--- apps/app_message.h
+++
@@ -1,9 +0,0 @@
-#ifndef _APP_MESSAGE_H_
-#define _APP_MESSAGE_H_
-
-int  app_messageWrite(char *buf ,int len);
-int  app_messageRead(void *buf,int len,int cmd);
-int  app_messageInit(void);
-void app_messageRelease(void);
-
-#endif
